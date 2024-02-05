f1716b3e93841913b7178cb0c48cf946abad493b
chandra8120 <chandrasekhar8120@gmail.com>
2024-02-05 18:36:37 +0530

    data add address

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ import dotenv from 'dotenv';
 import connectToMongoDB from './conn.js';
 import router from './routes.js';
 import { json } from 'express';
-import { v4 as uuidv4 } from 'uuid'
 import bcrypt from 'bcryptjs';
 import Login from './Model.js';
 import bodyParser from 'body-parser';
@@ -13,7 +12,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 app.use(cors());
-app.use(express.json({ limit: '10mb' })); 
+app.use(express.json({ limit: '10mb' }));
 connectToMongoDB();
 app.use(json());
 app.use('/', router);
@@ -21,7 +20,7 @@ app.use(bodyParser.json());
 
 app.post('/signup', async (req, res) => {
   try {
-    const { username, email, password, whatsapp, imageBase64,address } = req.body;
+    const { username, email, password, whatsapp, imageBase64, address } = req.body;
 
     const existingUser = await Login.findOne({ email });
 
@@ -31,7 +30,6 @@ app.post('/signup', async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-   
     const imageBuffer = Buffer.from(imageBase64, 'base64');
 
     const newUser = new Login({
@@ -39,8 +37,8 @@ app.post('/signup', async (req, res) => {
       email,
       password: hashedPassword,
       whatsapp,
-      image: imageBuffer.toString('base64'), 
-      address
+      image: imageBuffer.toString('base64'),
+      address,
     });
 
     await newUser.save();
@@ -51,9 +49,6 @@ app.post('/signup', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
-dotenv.config();
-
 
 app.post('/login', async (req, res) => {
   try {
@@ -76,10 +71,9 @@ app.post('/login', async (req, res) => {
       username: user.username,
       email: user.email,
       whatsapp: user.whatsapp,
-      image:user.image,
-      address:user.address
+      image: user.image,
+      address: user.address,
     };
-    console.log(userDataToSend,'userdata send,line 88')
     res.status(200).json({
       message: 'Login successful',
       user: userDataToSend,
@@ -89,7 +83,6 @@ app.post('/login', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
 
 app.get('/getAllUsers', async (req, res) => {
   try {
@@ -101,54 +94,21 @@ app.get('/getAllUsers', async (req, res) => {
   }
 });
 
-app.post('/reset-password/:resetToken', async (req, res) => {
-  try {
-    const { resetToken } = req.params;
-    const { newPassword } = req.body;
-
-    const user = await Login.findOne({
-      resetToken,
-      resetTokenExpiration: { $gt: new Date() },
-    });
-
-    if (!user) {
-      return res.status(401).json({ error: 'Invalid or expired reset token' });
-    }
-
-    const hashedPassword = await bcrypt.hash(newPassword, 10);
-    user.password = hashedPassword;
-    user.resetToken = null;
-    user.resetTokenExpiration = null;
-    await user.save();
-
-    res.status(200).json({ message: 'Password reset successful' });
-  } catch (error) {
-    console.error('Error resetting password:', error); // Log the error
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-
 app.post('/forgot-password', async (req, res) => {
   try {
-    const { email } = req.body;
+    const { email, whatsapp, newPassword } = req.body;
 
-    const user = await Login.findOne({ email });
+    const user = await Login.findOne({ email, whatsapp });
 
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
 
-    const resetToken = uuidv4();
-    const resetTokenExpiration = new Date(Date.now() + 3600000);
-
-    user.resetToken = resetToken;
-    user.resetTokenExpiration = resetTokenExpiration;
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedPassword;
     await user.save();
 
-    const resetLink = `http://localhost:3000/reset-password/${resetToken}`;
-
-    res.status(200).json({ message: 'Reset link sent successfully', resetToken });
+    res.status(200).json({ message: 'Password reset successful' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
@@ -186,11 +146,7 @@ app.delete('/deleteUser/:userId', async (req, res) => {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   });
-
 });
-
-
-
 
 let server;
 
