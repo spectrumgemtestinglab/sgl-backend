bcce911aad3afb17617843f3ae674cadb681b618
chandra8120 <chandrasekhar8120@gmail.com>
2024-02-08 11:57:48 +0530

    adding data login dehashed password

--- server/server.js
+++ server/server.js
@@ -121,6 +121,41 @@ app.post('/signup', async (req, res) => {
   }
 });
 
+// app.post('/login', async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+
+//     const user = await Login.findOne({ email });
+
+//     if (!user) {
+//       return res.status(404).json({ error: 'User not found' });
+//     }
+
+//     const isPasswordValid = await bcrypt.compare(password, user.password);
+
+//     if (!isPasswordValid) {
+//       return res.status(401).json({ error: 'Invalid password' });
+//     }
+
+//     const userDataToSend = {
+//       _id: user._id,
+//       username: user.username,
+//       email: user.email,
+//       whatsapp: user.whatsapp,
+//       image: user.image,
+//       address: user.address,
+//       password:user.password
+//     };
+//     res.status(200).json({
+//       message: 'Login successful',
+//       user: userDataToSend,
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
+
 app.post('/login', async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -137,6 +172,9 @@ app.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid password' });
     }
 
+    // Note: Storing the plaintext password temporarily (for demonstration purposes)
+    const plaintextPassword = password;
+
     const userDataToSend = {
       _id: user._id,
       username: user.username,
@@ -144,8 +182,9 @@ app.post('/login', async (req, res) => {
       whatsapp: user.whatsapp,
       image: user.image,
       address: user.address,
-      password:user.password
+      password: plaintextPassword
     };
+
     res.status(200).json({
       message: 'Login successful',
       user: userDataToSend,
