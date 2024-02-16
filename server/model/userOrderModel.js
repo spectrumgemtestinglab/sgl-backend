c9badf23562127d25453e4447f1194a204d59ce3
chandra8120 <chandrasekhar8120@gmail.com>
2024-02-16 10:14:06 +0530

    add payments

--- server/model/userOrderModel.js
+++ server/model/userOrderModel.js
@@ -3,14 +3,15 @@ import { Schema, model } from 'mongoose';
 const userOrderSchema = Schema({
   totalItems: { type: Number, required: true },
   totalCost: { type: Number, required: true },
-  shipping: { type: String, required: false },
+  shipping: { type: Boolean, required: true },
   grandTotal: { type: Number, required: true },
   username: { type: String, required: true },
   date: { type: Date, required: true },
   address: { type: String, required: true },
   status:{type:String,required:true},
   userID:{type:String,required:true},
-  number:{type:Number,required:true}
+  number:{type:Number,required:true},
+  payment:{type:String,required:true}
 });
 
 const UserOrders = model('UserOrders', userOrderSchema);
